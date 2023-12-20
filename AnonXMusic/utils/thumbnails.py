93a84370d9d694602aaa2070ea600abc78080f08
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-12-20 14:24:25 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -91,7 +91,7 @@ async def get_thumb(videoid):
         para = textwrap.wrap(title, width=32)
         j = 0
         draw.text(
-            (5, 5), "pihuxmusic", fill="white", font=name_font
+            (5, 5), "Rhythm X Music Bot", fill="white", font=name_font
         )
         draw.text(
             (600, 150),
