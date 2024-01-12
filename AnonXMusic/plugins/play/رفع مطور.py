a778a0c35bba2737786eede04badbca631436906
mvhamed <145144709+mvhamed@users.noreply.github.com>
2024-01-12 22:04:58 +0200

    Update رفع مطور.py

--- AnonXMusic/plugins/play/رفع مطور.py
+++ AnonXMusic/plugins/play/رفع مطور.py
@@ -6,8 +6,7 @@ import json
 from pyrogram import Client, filters
 from AnonXMusic import app
 
-casery = "mvhmed"
-DEVS =[f"{casery}"] 
+
 
 @app.on_message(filters.command("رفع مطور", "") & filters.group, group=5458658)
 async def mazojgvmbie(client, message):
