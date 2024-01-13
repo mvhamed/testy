dc251b42cf96c6a2726299c0e21c3bd036193957
mvhamed <145144709+mvhamed@users.noreply.github.com>
2024-01-13 19:29:11 +0200

    Update callback.py

--- AnonXMusic/plugins/admins/callback.py
+++ AnonXMusic/plugins/admins/callback.py
@@ -199,6 +199,7 @@ async def del_back_playlist(client, CallbackQuery, _):
         queued = check[0]["file"]
         title = (check[0]["title"]).title()
         user = check[0]["by"]
+        user_id = check[0]["user_id"]
         duration = check[0]["dur"]
         streamtype = check[0]["streamtype"]
         videoid = check[0]["vidid"]
@@ -226,7 +227,11 @@ async def del_back_playlist(client, CallbackQuery, _):
             except:
                 return await CallbackQuery.message.reply_text(_["call_6"])
             button = stream_markup(_, chat_id)
-            img = await get_thumb(videoid)
+            try:
+                photo = await client.download_media((await client.get_users(user_id)).photo.big_file_id)
+            except:
+                photo = await client.download_media((await client.get_users(app.id)).photo.big_file_id)
+            img = await get_thumb(videoid, photo)
             run = await CallbackQuery.message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
@@ -262,7 +267,11 @@ async def del_back_playlist(client, CallbackQuery, _):
             except:
                 return await mystic.edit_text(_["call_6"])
             button = stream_markup(_, chat_id)
-            img = await get_thumb(videoid)
+            try:
+                photo = await client.download_media((await client.get_users(user_id)).photo.big_file_id)
+            except:
+                photo = await client.download_media((await client.get_users(app.id)).photo.big_file_id)
+            img = await get_thumb(videoid, photo)
             run = await CallbackQuery.message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
@@ -333,7 +342,11 @@ async def del_back_playlist(client, CallbackQuery, _):
                 db[chat_id][0]["markup"] = "tg"
             else:
                 button = stream_markup(_, chat_id)
-                img = await get_thumb(videoid)
+                try:
+                    photo = await client.download_media((await client.get_users(user_id)).photo.big_file_id)
+                except:
+                    photo = await client.download_media((await client.get_users(app.id)).photo.big_file_id)
+                img = await get_thumb(videoid, photo)
                 run = await CallbackQuery.message.reply_photo(
                     photo=img,
                     caption=_["stream_1"].format(
@@ -394,3 +407,4 @@ async def markup_timer():
 
 
 asyncio.create_task(markup_timer())
+            
